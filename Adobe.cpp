61b30387c7153c15a2c0939c6af5c8264831bbef
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-01-14 16:43:34 +0530

    Adobe Questions

---
+++ Adobe.cpp
@@ -0,0 +1,10 @@
+/** Oral Questions
+1. Process vs Threads - Didn't prep OS. 
+Asked "So, you're not aware of Paging, Virual Memory,etc. "
+2. Hash Map - explain what are they (explaination could've been better)
+3. Favorite Search - Binary Search (Very versatile)
+*/
+
+
+//4. BST - construct using given numbers
+//5. DFS in grid
